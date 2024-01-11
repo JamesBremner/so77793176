25a13e5fac99d7234b86b1020bbfa57fb66d8297
JamesBremner <james@ravenspoint.com>
2024-01-11 11:16:46 -0500

    Code

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,136 @@
 #include <algorithm>
 #include <wex.h>
 #include "cStarterGUI.h"
+#include "cGraph.h"
+
+class cColor
+{
+public:
+    void generate1();
+    void generate2();
+    void color();
+
+private:
+    raven::graph::cGraph g;
+    std::vector<int> vColor;
+};
+
+void cColor::generate1()
+{
+    g.clear();
+    g.add("1", "2");
+    g.add("2", "3");
+    g.add("1", "3");
+}
+void cColor::generate2()
+{
+    g.clear();
+    g.add("1", "2");
+    g.add("1", "4");
+    g.add("1", "6");
+    g.add("3", "2");
+    g.add("3", "4");
+    g.add("3", "6");
+    g.add("2", "5");
+    g.add("4", "5");
+    g.add("7", "8");
+    g.add("7", "10");
+    g.add("7", "12");
+    g.add("9", "8");
+    g.add("9", "10");
+    g.add("9", "12");
+    g.add("8", "11");
+    g.add("10", "11");
+    g.add("13", "14");
+    g.add("13", "16");
+    g.add("13", "18");
+    g.add("15", "14");
+    g.add("15", "16");
+    g.add("15", "18");
+    g.add("14", "17");
+    g.add("16", "17");
+    g.add("5", "11");
+    g.add("12", "17");
+    g.add("18", "6");
+
+
+}
+
+void cColor::color()
+{
+    vColor.clear();
+    vColor.resize(2 * g.edgeCount(), 1);
+
+    while (true) // loop A
+    {
+        int max = 0;
+        int maxEdge = 0;
+
+        for (int E = 0; E < g.edgeCount(); E++)
+        {
+            if (vColor[E] != 1)
+                continue;
+
+            int S = 0;
+
+            int vsrc = g.src(E);
+            auto va = g.adjacentOut(vsrc);
+            if (va.size() == 1)
+                continue;
+            for (int v : va)
+            {
+                int ei = g.find(vsrc, v);
+                if (vColor[ei] != 1)
+                    continue;
+                S++;
+            }
+            va = g.adjacentOut(g.dest(E));
+            if (va.size() == 1)
+                continue;
+            for (int v : va)
+            {
+                int ei = g.find(E, v);
+                if (vColor[ei] != 1)
+                    continue;
+                S++;
+            }
+
+            if (S > max)
+            {
+                max = S;
+                maxEdge = E;
+            }
+        } // end loop E
+
+        if (max == 0)
+        {
+
+            // finished
+
+            int se = 0;
+            for (auto e : g.edgeList())
+            {
+                int ei = g.find(e.first, e.second);
+                se += vColor[ei];
+
+                std::cout 
+                          << g.userName(g.src(ei))
+                          << " " << g.userName(g.dest(ei))
+                          << " color " << vColor[ei]
+                          << "\n";
+            }
+            std::cout << "sum of all edge colors " << se << "\n";
+            return;
+        }
+
+        for (int v : g.adjacentOut(g.src(maxEdge)))
+            vColor[g.find(maxEdge, v)] = 0;
+        for (int v : g.adjacentOut(g.dest(maxEdge)))
+            vColor[g.find(maxEdge, v)] = 0;
+        vColor[maxEdge] = 2;
+
+    } // end loop A
+}
 
 class cGUI : public cStarterGUI
 {
@@ -13,18 +143,20 @@ public:
     cGUI()
         : cStarterGUI(
               "Starter",
-              {50, 50, 1000, 500}),
-          lb(wex::maker::make < wex::label >(fm))
+              {50, 50, 1000, 500})
     {
-        lb.move(50, 50, 100, 30);
-        lb.text("Hello World");
+        // color.generate1();
+        // color.color();
+        // std::cout << "==================================\n";
+        color.generate2();
+        color.color();
 
         show();
         run();
     }
 
 private:
-    wex::label &lb;
+    cColor color;
 };
 
 main()
